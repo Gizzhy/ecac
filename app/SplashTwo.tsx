4240c81c83487f3bb0274ff3658a90d8462ebf6f
Gizzhy <gizzhyakinz@gmail.com>
2025-06-18 23:25:26 +0100

    splashscreen two

--- app/SplashTwo.tsx
+++ app/SplashTwo.tsx
@@ -1,7 +1,9 @@
 // app/splash2.tsx
 import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, SafeAreaView, View } from 'react-native';
+import TText from './components/StyledText';
+
 
 export default function SplashTwo() {
   const router = useRouter();
@@ -15,23 +17,29 @@ export default function SplashTwo() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Loading your experience...</Text>
-      <ActivityIndicator size="large" color="#00bcd4" style={{ marginTop: 20 }} />
-    </View>
+    <ImageBackground
+      source={require('../assets/images/splashimg.jpg')} 
+      resizeMode="cover"
+      className="flex-1 justify-center items-center"
+    >
+        <SafeAreaView >
+        <View className='flex flex-col items-center h-[100%] justify-evenly'>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR VISION</TText>
+                <TText className='text-center'>To proclaim the Gospel of Jesus Christ to all people in a comprehensive way from our locality to the rest of the world.</TText>
+            </View>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR MISSION</TText>
+                <TText className='text-center'>Our mission is to prepare the people for the kingdom of God through Evangelism, Christian Education and Christ love.</TText>
+            </View>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR CORE VALUES</TText>
+                <TText className='text-center'>Holiness & Soul Winning</TText>
+            </View>
+        </View>
+        </SafeAreaView>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0f172a',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    color: '#fff',
-    fontWeight: '500',
-  },
-});
+
--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,5 @@
 // app/home.tsx
-import { Image, ImageBackground, SafeAreaView, View } from 'react-native';
+import { ImageBackground, SafeAreaView, View } from 'react-native';
 import TText from './components/StyledText';
 
 export default function HomeScreen() {
@@ -11,26 +11,20 @@ export default function HomeScreen() {
     >
         <SafeAreaView >
         <View className='flex flex-col items-center h-[100%] justify-evenly'>
-        <View className=' flex flex-col items-center '>
-        <Image
-        source={require('../assets/images/ecac.heic')}
-        className="w-56 h-56 rounded-full mb-8"
-      />
-      <TText className="text-black text-[24px] font-montserrat-semibold">Preaching The Gospel...</TText>
-        </View>
-        
-      <View className=' flex flex-col items-center'>
-        <TText className='text-[#074975] text-[32px] mb-4 font-montserrat-semibold'>Sunday School</TText>
-        <TText className='text-[#931414] text-[24px] font-montserrat-semibold'>For Year 2025</TText>
-      </View>
-      <View className='flex flex-col items-center'>
-        <TText className='text-[#074975] text-[32px] font-montserrat-semibold'>Essentials For</TText>
-        <TText className='text-[#074975] text-[32px] font-montserrat-semibold'>Christian Living</TText>
-      </View>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR VISION</TText>
+                <TText className='text-center'>To proclaim the Gospel of Jesus Christ to all people in a comprehensive way from our locality to the rest of the world.</TText>
+            </View>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR MISSION</TText>
+                <TText className='text-center'>Our mission is to prepare the people for the kingdom of God through Evangelism, Christian Education and Christ love.</TText>
+            </View>
+            <View className='px-10'>
+                <TText className='text-center text-[20px] mb-4 font-montserrat-semibold'>OUR CORE VALUES</TText>
+                <TText className='text-center'>Holiness & Soul Winning</TText>
+            </View>
         </View>
         </SafeAreaView>
-        
-        
     </ImageBackground>
   );
 }
